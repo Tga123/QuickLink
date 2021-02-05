dab62aa8163358f2e103b6208ede9cd72944fb9b
Tga123 <29838841+Tga123@users.noreply.github.com>
2021-02-05 17:26:27 +0300

    Minor improvement.

--- Modules/bnet.lua
+++ Modules/bnet.lua
@@ -3,7 +3,7 @@ local QuickLink_BNET = QuickLink:NewModule("QuickLink_BNET", "AceConsole-3.0", "
 local L = LibStub("AceLocale-3.0"):GetLocale("QuickLink", true)
 
 -- There must be a better way!
-function QuickLink_BNET:FriendsFrame_ShowBNDropdown(name, connected, lineID, chatType, chatFrame, friendsList, bnetIDAccount)
+function QuickLink_BNET:FriendsFrame_ShowBNDropdown(name, connected, lineID, chatType, chatFrame, friendsList, bnetIDAccount, communityClubID, communityStreamID, communityEpoch, communityPosition, mobile)
 	local index = nil
 
 	local client = C_BattleNet.GetAccountInfoByID(bnetIDAccount).gameAccountInfo.clientProgram
@@ -22,7 +22,7 @@ function QuickLink_BNET:FriendsFrame_ShowBNDropdown(name, connected, lineID, cha
 			table.remove(UnitPopupMenus["BN_FRIEND"], index)
 		end
 	end
-	self.hooks["FriendsFrame_ShowBNDropdown"](name, connected, lineID, chatType, chatFrame, friendsList, bnetIDAccount);
+	self.hooks["FriendsFrame_ShowBNDropdown"](name, connected, lineID, chatType, chatFrame, friendsList, bnetIDAccount, communityClubID, communityStreamID, communityEpoch, communityPosition, mobile);
 end
 
 function QuickLink_BNET:OnEnable()
